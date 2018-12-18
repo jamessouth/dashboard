670d49ed5c14fb18732b909b926003e0597c3dc6
jamessouth <jamesdanielsouth@gmail.com>
2018-12-18 03:39:38 -0600

    forgotten files

---
+++ docs/precache-manifest.5bf49f08487cfa6c63eb2ee1ae624c26.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df5f6f4c93a9944d6c03cf5bf586a6c",
+    "url": "/Vue-Project-9/img/icon-settings.3df5f6f4.svg"
+  },
+  {
+    "revision": "ee3ce4a72d349ac7fe592f6b4207c28b",
+    "url": "/Vue-Project-9/index.html"
+  },
+  {
+    "revision": "408c66d5ddab17da293e",
+    "url": "/Vue-Project-9/js/chunk-vendors-legacy.b0fe8568.js"
+  },
+  {
+    "revision": "487fc8058b9d4dff7db0",
+    "url": "/Vue-Project-9/js/app-legacy.539975a0.js"
+  },
+  {
+    "revision": "8d0aea6feebb057ff0d8e3134d77d201",
+    "url": "/Vue-Project-9/img/nodata.8d0aea6f.png"
+  },
+  {
+    "revision": "9da17580cca235ecd3f8a54de7ed346e",
+    "url": "/Vue-Project-9/img/icon-visits.9da17580.svg"
+  },
+  {
+    "revision": "a7fdf16a451bac72c28ac797e2876b4f",
+    "url": "/Vue-Project-9/img/icon-members.a7fdf16a.svg"
+  },
+  {
+    "revision": "6e9bdcd2c3d0974dc83c5074e2f97d75",
+    "url": "/Vue-Project-9/img/icon-dashboard.6e9bdcd2.svg"
+  },
+  {
+    "revision": "692327889865deed569b3a9f9127b822",
+    "url": "/Vue-Project-9/img/face.69232788.jpg"
+  },
+  {
+    "revision": "bd558a77e83a0fffe77d67b2bbc21f94",
+    "url": "/Vue-Project-9/img/downArrow.bd558a77.jpg"
+  },
+  {
+    "revision": "6196afe19dcdcac45a5713d7e9891fee",
+    "url": "/Vue-Project-9/fonts/UpperEastSide-webfont.6196afe1.woff"
+  },
+  {
+    "revision": "487fc8058b9d4dff7db0",
+    "url": "/Vue-Project-9/css/app.26fab197.css"
+  }
+];
---
+++ docs/precache-manifest.e0fd33245b7b0fc3acc1d0b1d264b198.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df5f6f4c93a9944d6c03cf5bf586a6c",
+    "url": "/Vue-Project-9/img/icon-settings.3df5f6f4.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-Project-9/robots.txt"
+  },
+  {
+    "revision": "6e9bdcd2c3d0974dc83c5074e2f97d75",
+    "url": "/Vue-Project-9/img/icon-dashboard.6e9bdcd2.svg"
+  },
+  {
+    "revision": "ffd408249a3c50506bb9",
+    "url": "/Vue-Project-9/js/chunk-vendors.72c806d7.js"
+  },
+  {
+    "revision": "cee1f70c34051a333863",
+    "url": "/Vue-Project-9/js/app.42b65ede.js"
+  },
+  {
+    "revision": "056721ca0f31b88e91fa48797d2d8561",
+    "url": "/Vue-Project-9/index.html"
+  },
+  {
+    "revision": "8d0aea6feebb057ff0d8e3134d77d201",
+    "url": "/Vue-Project-9/img/nodata.8d0aea6f.png"
+  },
+  {
+    "revision": "9da17580cca235ecd3f8a54de7ed346e",
+    "url": "/Vue-Project-9/img/icon-visits.9da17580.svg"
+  },
+  {
+    "revision": "a7fdf16a451bac72c28ac797e2876b4f",
+    "url": "/Vue-Project-9/img/icon-members.a7fdf16a.svg"
+  },
+  {
+    "revision": "692327889865deed569b3a9f9127b822",
+    "url": "/Vue-Project-9/img/face.69232788.jpg"
+  },
+  {
+    "revision": "bd558a77e83a0fffe77d67b2bbc21f94",
+    "url": "/Vue-Project-9/img/downArrow.bd558a77.jpg"
+  },
+  {
+    "revision": "6196afe19dcdcac45a5713d7e9891fee",
+    "url": "/Vue-Project-9/fonts/UpperEastSide-webfont.6196afe1.woff"
+  },
+  {
+    "revision": "cee1f70c34051a333863",
+    "url": "/Vue-Project-9/css/app.26fab197.css"
+  }
+];
