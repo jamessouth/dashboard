9ec6ae055ba9d91bca170b6295329d8dde0c1ee8
jamessouth <jamesdanielsouth@gmail.com>
2019-01-12 00:28:22 -0600

    sw work

--- web-app-dashboard/src/service-worker.js
+++ web-app-dashboard/src/service-worker.js
@@ -9,7 +9,7 @@ const suffix = 'v1';
 workbox.core.setCacheNameDetails({ prefix, suffix });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings(false);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener('message', (e) => {
@@ -18,32 +18,32 @@ self.addEventListener('message', (e) => {
   if (e.data === 'skipWaiting') self.skipWaiting();
 });
 
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: `${prefix}-google-fonts-css`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
+// workbox.routing.registerRoute(
+//   /^https:\/\/fonts\.googleapis\.com/,
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: `${prefix}-google-fonts-css`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );17
 
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  workbox.strategies.cacheFirst({
-    cacheName: `${prefix}-google-fonts-webfonts`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
-
+// workbox.routing.registerRoute(
+//   /^https:\/\/fonts\.gstatic\.com/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: `${prefix}-google-fonts-webfonts`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );<1
+//
 workbox.routing.registerRoute(
   /^https:\/\/eonet\.sci\.gsfc\.nasa\.gov\/api\/v2\.1\/events\?days=60/,
   workbox.strategies.networkFirst({
@@ -56,77 +56,80 @@ workbox.routing.registerRoute(
     ],
   }),
 );
+//
+// workbox.routing.registerRoute(
+//   /^https:\/\/randomuser\.me\/api\/\?results=10&inc=name,email,picture&noinfo/,
+//   workbox.strategies.networkFirst({
+//     cacheName: `${prefix}-randomuser-data`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );
+//
+// workbox.routing.registerRoute(
+//   /^https:\/\/en\.wikipedia\.org\/w\/api\.php\?action=parse&page=Time_zone&prop=text&section=11&format=json&origin=\*/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: `${prefix}-wikipedia-timezones`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );
+//
+// workbox.routing.registerRoute(
+//   /^https:\/\/api\.worldbank\.org\/v2\/countries/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: `${prefix}-worldbank-data`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );
+//
+// workbox.routing.registerRoute(
+//   /^https:\/\/randomuser\.me\/api\/portraits/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: `${prefix}-randomuser-pics`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 50,
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         purgeOnQuotaError: true,
+//       }),
+//       new workbox.cacheableResponse.Plugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   }),
+// );
+//
+// workbox.routing.registerRoute(
+//   /nodata\.\w*\.?png$/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: `${prefix}-unprecached`,
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxAgeSeconds: 60 * 60 * 24 * 365,
+//         purgeOnQuotaError: true,
+//       }),
+//     ],
+//   }),
+// );
 
-workbox.routing.registerRoute(
-  /^https:\/\/randomuser\.me\/api\/\?results=10&inc=name,email,picture&noinfo/,
-  workbox.strategies.networkFirst({
-    cacheName: `${prefix}-randomuser-data`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
+// workbox.routing.setDefaultHandler(workbox.strategies.staleWhileRevalidate());
 
-workbox.routing.registerRoute(
-  /^https:\/\/en\.wikipedia\.org\/w\/api\.php\?action=parse&page=Time_zone&prop=text&section=11&format=json&origin=\*/,
-  workbox.strategies.cacheFirst({
-    cacheName: `${prefix}-wikipedia-timezones`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
-
-workbox.routing.registerRoute(
-  /^https:\/\/api\.worldbank\.org\/v2\/countries/,
-  workbox.strategies.cacheFirst({
-    cacheName: `${prefix}-worldbank-data`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 30,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
-
-workbox.routing.registerRoute(
-  /^https:\/\/randomuser\.me\/api\/portraits/,
-  workbox.strategies.cacheFirst({
-    cacheName: `${prefix}-randomuser-pics`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        purgeOnQuotaError: true,
-      }),
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-    ],
-  }),
-);
-
-workbox.routing.registerRoute(
-  /nodata\.\w*\.?png$/,
-  workbox.strategies.cacheFirst({
-    cacheName: `${prefix}-unprecached`,
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        purgeOnQuotaError: true,
-      }),
-    ],
-  }),
-);
-
-workbox.routing.setDefaultHandler(workbox.strategies.staleWhileRevalidate());
-
-workbox.routing.setCatchHandler(() => Response.error());
+// workbox.routing.setCatchHandler(() => {
+//   console.log('error route called');
+//   Response.error();
+// });
